21b9f8c18d27969f8b66dd07e74525772e8a4b5f
gregorhenning <gregorhenning@gmx.de>
2024-04-26 15:08:47 +0200

    New Exercise Steigung

---
+++ untitled3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 14:51:58 2024
+
+@author: s_henningg20
+"""
+
+def steigung_funktion(liste):
+        x1 = liste [0]
+        y1 = liste [1]
+        x2 = liste [2]
+        y2 = liste [3]
+        
+        if (x2-x1) == 0:
+            print("Die Steigung ist nicht definiert")
+        else:
+            print((y2-y1)/(x2-x1))
+            
+            
+    
+        
+        
+    
