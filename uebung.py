32af72eb8eff34f71b535ef20db9af2a448e91b9
gregorhenning <gregorhenning@gmx.de>
2024-04-26 14:31:02 +0200

    Uebung Exercise if else

---
+++ uebung.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 13:32:26 2024
+
+@author: s_henningg20
+"""
+
+def rechnen(x,y):
+        if x ==y : 
+            print("x und y sind gleich")
+        elif y == 0 : 
+            print("Es ist nicht möglich durch 0 zu teilen")
+        else:
+            if x %y == 0:
+                print("x ist durch y teilbar")
+            else:
+                print("x ist nicht durch y teilbar") 
+            
+            
+            
+
+
+    
